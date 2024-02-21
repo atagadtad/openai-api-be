e46de19ad90bb1806d75cdca6c390496e021890f
aaron <aarontagadtad@gmail.com>
2024-02-21 13:38:16 -0500

    added nodemon library. changed request to post

--- index.js
+++ index.js
@@ -15,13 +15,15 @@ const port = 3000;
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get("/", async (req, res) => {
+app.post("/message", async (req, res) => {
+  console.log(req.body);
+
   const completion = await openai.chat.completions.create({
     messages: [{ role: "user", content: "Hello world!" }],
     model: "gpt-3.5-turbo",
   });
 
-  res.json(completion.choices[0]);
+  return res.json(completion.choices[0]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
